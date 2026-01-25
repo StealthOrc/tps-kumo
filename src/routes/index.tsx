f97c54956d58cd418c079d3eed98bf9871d5056f
StealthOrc <nitram1999@hotmail.de>
2026-01-25 01:08:18 +0100

    enabled react strict mode

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { StrictMode } from "react";
 import TpsHistory from "@/components/tpsHistory";
 import { getTPS } from "@/data/tps";
 
@@ -7,18 +8,20 @@ export const Route = createFileRoute("/")({ component: App });
 function App() {
 	const tps = getTPS();
 	return (
-		<div className="flex flex-col p-5 gap-4 items-center min-h-screen bg-[hsl(216,31.3%,12.5%)] text-white">
-			<div className="bg-[hsl(214,43%,21%)] border-4 border-[hsl(224,15%,20%)] rounded-sm p-2">
-				<h1 className="text-6xl text-center">TPS Kumo</h1>
-				<p className="text-xl px-2">
-					TPS Kumo is a tool for monitoring TPS of a Hytale Server.
-				</p>
+		<StrictMode>
+			<div className="flex flex-col p-5 gap-4 items-center min-h-screen bg-[hsl(216,31.3%,12.5%)] text-white">
+				<div className="bg-[hsl(214,43%,21%)] border-4 border-[hsl(224,15%,20%)] rounded-sm p-2">
+					<h1 className="text-6xl text-center">TPS Kumo</h1>
+					<p className="text-xl px-2">
+						TPS Kumo is a tool for monitoring TPS of a Hytale Server.
+					</p>
+				</div>
+				<div className="flex flex-col gap-2">
+					<TpsHistory title="10s" tps={tps} />
+					<TpsHistory title="5min" tps={tps} />
+					<TpsHistory title="10min" tps={tps} />
+				</div>
 			</div>
-			<div className="flex flex-col gap-2">
-				<TpsHistory title="10s" tps={tps} />
-				<TpsHistory title="5min" tps={tps} />
-				<TpsHistory title="10min" tps={tps} />
-			</div>
-		</div>
+		</StrictMode>
 	);
 }
